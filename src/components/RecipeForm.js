258bae3b9d1994afb0ccaf5f650060ea8b5c15a3
David Casanellas <davidcasanell@gmail.com>
2018-06-07 20:39:19 +0200

    feat: added Recipe and RecipeForm components

---
+++ src/components/RecipeForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { createRecipe } from '../actions/recipeActions';
+
+class RecipeForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      difficulty: ''
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const recipe = {
+      name: this.state.name,
+      difficulty: this.state.difficulty
+    }
+
+    this.props.createRecipe(recipe);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add Recipe</h1>
+        <form onSubmit={ this.onSubmit }>
+          <div>
+            <label>Name: </label><br />
+            <input type="text" name="name" onChange={ this.onChange } value={ this.state.name } />
+          </div>
+          <br />
+          <div>
+            <label>Difficult: </label><br />
+            <textarea name="difficulty" onChange={ this.onChange } value={ this.state.difficulty }/>
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
+
+
+
+
+
+
+
+
+}
---
+++ src/components/Recipes.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { fetchRecipes } from '../actions/postActions';
+
+class Recipes extends Component {
+  componentWillMount() {
+    this.props.fetchRecipes();
+  }
+
+  render() {
+    const recipeItems = this.props.recipes.map(recipe => (
+      <div key={ recipe.id }>
+        <h3>{ recipe.name }</h3>
+      </div>
+    ));
+
+    return (
+      <div>
+        <h1>Recipes</h1>
+        { recipeItems }
+      </div>
+    )
+  }
+}
+
+Recipes.propTypes = {
+  fetchRecipes: PropTypes.func.isRequired,
+  recipes: PropTypes.array.isRequired,
+  newRecipe: PropTypes.object
+}
+
+const mapStateToProps = state => ({
+  // recipesReducer is called recipes in combineRecuders
+  recipes: state.recipes.items,
+  newRecipe: state.recipes.item
+})
+
+export default connect(mapStateToProps, { fetchRecipes })(Recipes);
