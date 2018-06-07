1ccf00de57e4e1437d3a333f609f338d044caa44
David Casanellas <davidcasanell@gmail.com>
2018-06-07 22:27:56 +0200

    chore: add Provider + store and Recipes + RecipeForm components in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import Recipes from './components/Recipes';
+import RecipeForm from './components/RecipeForm';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Welcome to recipes!
-      </div>
+      <Provider store={ store }>
+        <div className="App">
+          <header className="App-header">
+            <img src={ logo } className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <h4>Made with ♥ from Torcy, FR</h4>
+          </header>
+          <RecipeForm />
+          <hr />
+          <Recipes />
+        </div>
+      </Provider>
     )
   }
 }
