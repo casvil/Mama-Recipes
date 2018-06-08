ab51aa0f827e3799b069663f386112520d67b784
David Casanellas <davidcasanell@gmail.com>
2018-06-08 20:39:06 +0200

    feat: add new filtering case in reducer for searching recipes suggestions

--- src/reducers/recipeReducer.js
+++ src/reducers/recipeReducer.js
@@ -1,23 +1,32 @@
-import { FETCH_RECIPES, NEW_RECIPE } from '../actions/types';
+import { FETCH_RECIPES, NEW_RECIPE, SEARCH_RECIPE } from '../actions/types';
 
 const initialState = {
   items: [],
-  item: {}
-}
+  item: {},
+  suggestions: []
+};
 
-export default function (state = initialState, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_RECIPES:
       return {
         ...state,
         items: action.payload
-      }
+      };
 
     case NEW_RECIPE:
       return {
         ...state,
         item: action.payload
-      }
+      };
+
+    case SEARCH_RECIPE:
+      return {
+        ...state,
+        suggestions: state.items.filter(item =>
+          item.name.toLowerCase().includes(action.payload.toLowerCase())
+        )
+      };
 
     default:
       return state;
