7f7fcc6df0a1721793341181e34c48bfc279a507
David Casanellas <davidcasanell@gmail.com>
2018-06-10 01:19:20 +0200

    fix: allow Redux devtools only for Chrome

--- src/store.js
+++ src/store.js
@@ -4,15 +4,16 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
-const middleware = [
-  thunk
-];
+const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.navigator.userAgent.includes('Chrome')
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      : compose
   )
 );
 
