47a373ff8deb4719a38f92ddfc418b90ab907a5e
David Casanellas <davidcasanell@gmail.com>
2018-06-22 23:49:07 +0200

    feat: create history as routerReducer

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
 const initialState = {};
-const middleware = [thunk];
-const store = createStore(
-  rootReducer,
+export const history = createBrowserHistory(); // history object provided to ConnectedRouter
+const middleware = [thunk, routerMiddleware(history)];
+export const store = createStore(
+  connectRouter(history)(rootReducer), // new root reducer with router state
   initialState,
   compose(
     applyMiddleware(...middleware),
@@ -16,5 +19,3 @@ const store = createStore(
       : compose
   )
 );
-
-export default store;
