eed8dd3f3b1d2970ede2e2b2e9d58db4b0adf4b1
David Casanellas <davidcasanell@gmail.com>
2018-06-23 02:56:14 +0200

    chore: single export file module for all Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Recipes from './components/Recipes';
-import RecipeForm from './components/RecipeForm';
-import Search from './components/Search';
+import Components from './components/routed-components';
 
 import './App.css';
 
@@ -15,17 +11,19 @@ class App extends Component {
       <Switch>
         <div className="app">
           <div className="app__header">
-            <Route path="/" component={Header} />
+            <Route path="/" component={Components.Header} />
           </div>
           <div className="app__form-search">
-            <Route path="/" component={Search} />
+            <Route path="/" component={Components.Search} />
           </div>
           <div className="app__content">
-            <Route path="/recipe" component={RecipeForm} />
-            <Route path="/" component={Recipes} />
+            <Route path="/register" component={Components.UserRegister} />
+            <Route path="/login" component={Components.Login} />
+            <Route path="/recipe" component={Components.RecipeForm} />
+            <Route path="/" component={Components.Recipes} />
           </div>
           <div className="app__footer">
-            <Route path="/" component={Footer} />
+            <Route path="/" component={Components.Footer} />
           </div>
         </div>
       </Switch>
---
+++ src/components/routed-components.js
@@ -0,0 +1,19 @@
+import Header from './Header';
+import Footer from './Footer';
+import Recipes from './Recipes';
+import RecipeForm from './RecipeForm';
+import Search from './Search';
+import UserRegister from './UserRegister';
+import Login from './Login';
+
+const Components = {
+  Header,
+  Footer,
+  Recipes,
+  RecipeForm,
+  Search,
+  UserRegister,
+  Login
+};
+
+export default Components;
