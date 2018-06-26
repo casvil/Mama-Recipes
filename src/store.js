11bbb8afc85001f59b0b763698232e7d55aeca81
David Casanellas <davidcasanell@gmail.com>
2018-06-27 00:12:47 +0200

    fix: devtools only enabled in NODE_ENV=development

--- src/store.js
+++ src/store.js
@@ -8,14 +8,15 @@ import rootReducer from './reducers';
 const initialState = {};
 export const history = createBrowserHistory(); // history object provided to ConnectedRouter
 const middleware = [thunk, routerMiddleware(history)];
+
 export const store = createStore(
   connectRouter(history)(rootReducer), // new root reducer with router state
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.navigator.userAgent.includes('Chrome')
-      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-      : compose
-  )
+  process.env.NODE_ENV === 'development'
+    ? compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    : applyMiddleware(...middleware)
 );
