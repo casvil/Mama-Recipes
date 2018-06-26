b1000137267cf4a76975ea6e8ecfd04978007ef0
David Casanellas <davidcasanell@gmail.com>
2018-06-27 01:28:25 +0200

    fix: devtools

--- src/store.js
+++ src/store.js
@@ -14,7 +14,6 @@ export const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    process.env.NODE_ENV !== 'production' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
       ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
       : compose
