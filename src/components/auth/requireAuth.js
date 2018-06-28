b22c7fa2521846df68d1711a45aa8deb5d9c7669
David Casanellas <davidcasanell@gmail.com>
2018-06-28 18:15:49 +0200

    chore: auth folder and renamed its components

---
+++ src/components/auth/requireAuth.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default ChildComponent => {
+  class ComposedComponent extends Component {
+    // Our component just got rendered
+    componentDidMount() {
+      this.shouldNavigateAway();
+    }
+    // Our component just got updated
+    componentDidUpdate() {
+      this.shouldNavigateAway();
+    }
+    shouldNavigateAway() {
+      if (!this.props.authToken) {
+        this.props.history.push('/');
+      }
+    }
+    render() {
+      return <ChildComponent {...this.props} />;
+    }
+  }
+  function mapStateToProps(state) {
+    return { authToken: state.user.authToken };
+  }
+  return connect(mapStateToProps)(ComposedComponent);
+};
