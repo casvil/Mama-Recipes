af8218b8ad6914af0e7fd31ff7f3e5af44648a1c
David Casanellas <davidcasanell@gmail.com>
2018-07-01 21:51:10 +0200

    chore: stateful component Recipe

---
+++ src/components/recipe/Recipe.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+export const Recipe = props => {
+  return (
+    <div className="recipes__recipe" key={props.recipe._id}>
+      <h3 className="recipe__name">{props.recipe.name}</h3>
+      <ul className="recipe__ingredientList">
+        {props.recipe.ingredients.map((ingredient, id) => (
+          <li className="recipe__ingredient" key={id}>
+            {ingredient}
+            {id}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
