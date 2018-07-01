4b1d23c5edeb487162bf1d6e2278b48999b853f1
David Casanellas <davidcasanell@gmail.com>
2018-07-01 22:35:16 +0200

    chore: mockLocalStorage function

---
+++ src/helpers/mockLocalStorage.js
@@ -0,0 +1,23 @@
+// Storage Mock
+export default function storageMock() {
+  var storage = {};
+
+  return {
+    setItem: function(key, value) {
+      storage[key] = value || '';
+    },
+    getItem: function(key) {
+      return key in storage ? storage[key] : null;
+    },
+    removeItem: function(key) {
+      delete storage[key];
+    },
+    get length() {
+      return Object.keys(storage).length;
+    },
+    key: function(i) {
+      var keys = Object.keys(storage);
+      return keys[i] || null;
+    }
+  };
+}
