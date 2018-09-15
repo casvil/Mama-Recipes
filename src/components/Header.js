1425ddde0cf0ff897e92eaaed7ed864e96bad9f9
David Casanellas <davidcasanell@gmail.com>
2018-09-15 20:26:31 +0200

    feat: remove custom CSS for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,44 +2,49 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Navbar, Button, Alignment } from '@blueprintjs/core';
 
 import { signOut } from '../actions/authActions';
-
-import './header.css';
 
 class Header extends Component {
   renderLinks() {
     if (this.props.auth.authenticated) {
       return (
-        <header className="header">
-          <Link to="/" className="header__title">
-            <h1>Mama Recipes</h1>
-          </Link>
-          <Link to="/recipe" className="header__item">
-            <h1>New Recipe</h1>
-          </Link>
-          <Link
-            className="header__item"
-            to="/signout"
-            onClick={this.props.signOut}
-          >
-            <h1>Signout</h1>
-          </Link>
-        </header>
+        <Navbar>
+          <Navbar.Group align={Alignment.LEFT}>
+            <Navbar.Heading>
+              <Link to="/">Mama Recipes</Link>
+            </Navbar.Heading>
+            <Navbar.Divider />
+          </Navbar.Group>
+          <Navbar.Group align={Alignment.RIGHT}>
+            <Link to="/recipe">
+              <Button className="bp3-minimal" icon="add" text="New recipe" />
+            </Link>
+            <Link to="/signout" onClick={this.props.signOut}>
+              <Button className="bp3-minimal" icon="log-out" text="Sign out" />
+            </Link>
+          </Navbar.Group>
+        </Navbar>
       );
     } else {
       return (
-        <header className="header">
-          <Link to="/" className="header__title">
-            <h1>Mama Recipes</h1>
-          </Link>
-          <Link to="/signup" className="header__item">
-            <h1>Sign Up</h1>
-          </Link>
-          <Link to="/signin" className="header__item">
-            <h1>Sign In</h1>
-          </Link>
-        </header>
+        <Navbar>
+          <Navbar.Group align={Alignment.LEFT}>
+            <Navbar.Heading>
+              <Link to="/">Mama Recipes</Link>
+            </Navbar.Heading>
+            <Navbar.Divider />
+          </Navbar.Group>
+          <Navbar.Group align={Alignment.RIGHT}>
+            <Link to="/signup">
+              <Button className="bp3-minimal" icon="user" text="Sign Up" />
+            </Link>
+            <Link to="/signin">
+              <Button className="bp3-minimal" icon="log-in" text="Sign In" />
+            </Link>
+          </Navbar.Group>
+        </Navbar>
       );
     }
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { AppContainer } from 'react-hot-loader';
 import dotenv from 'dotenv';
+import { FocusStyleManager } from '@blueprintjs/core';
 
 import { store, history } from './store/store';
 import registerServiceWorker from './registerServiceWorker';
@@ -12,6 +13,8 @@ import './index.css';
 import App from './App';
 
 dotenv.config();
+
+FocusStyleManager.onlyShowFocusOnTabs();
 
 ReactDOM.render(
   <AppContainer>
